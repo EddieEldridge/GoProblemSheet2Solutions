6cb0562456332cb98919d905c2e6e5a0ab058a20
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2017-10-27 21:29:27 +0100

    Re-organized

---
+++ Problem 3/bootstrap.go
@@ -0,0 +1,23 @@
+// Go Code written by Edward Eldridge (G00337490)
+// GMIT - Software Development
+// Problem 3 taken from https://data-representation.github.io/problems/go-web-applications.html
+// Code adapted from https://getbootstrap.com/docs/4.0/getting-started/introduction/#starter-template and https://stackoverflow.com/questions/26559557/how-do-you-serve-a-static-html-file-using-a-go-web-server
+
+
+package main
+
+import (
+	//"fmt"
+	"net/http"
+)
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+
+	//Browser renders html tags
+	w.Header().Set("Content-Type","text/html")
+}
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":8080", nil)
+}
